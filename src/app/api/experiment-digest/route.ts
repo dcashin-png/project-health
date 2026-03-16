ab023ed5cc6c6ff1f7335f9d3c103d3a3e3a2679
Danny Cashin <dcashin@slack-corp.com>
2026-03-16 16:23:48 -0400

    Revert "fix: Filter out Cancelled experiments and Won't Do resolutions"
    This reverts commit 6aef5faddd59a5677070246546507617afe9de1f.

--- src/app/api/experiment-digest/route.ts
+++ src/app/api/experiment-digest/route.ts
@@ -63,8 +63,7 @@ export async function GET() {
 
     const JQL = `project IN (${PROJECTS})
       AND issuetype in (Epic, Experiment)
-      AND "Experiment Status" NOT IN ("Cancelled")
-      AND resolution NOT IN ("Won't Do")
+      AND status NOT IN (Cancelled, "Won't Do")
       AND (
         ("Experiment Start Date" >= "${dates.monthStart}" AND "Experiment Start Date" <= "${dates.nextMonthEnd}")
         OR ("Experiment End Date" >= "${dates.lastWeekStart}" AND "Experiment End Date" <= "${dates.thisWeekEnd}")
